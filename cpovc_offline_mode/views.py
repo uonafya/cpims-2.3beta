6cec024f38fb66f8e8307fe68298e63b81e8991e
Collins Liyosi <collinskivale@gmail.com>
2019-07-09 15:06:30 +0300

    put some upper bound to queries written

--- cpovc_offline_mode/views.py
+++ cpovc_offline_mode/views.py
@@ -5,8 +5,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 from cpovc_auth.functions import get_attached_units
-from cpovc_ovc.models import OVCRegistration
+from cpovc_ovc.models import OVCRegistration, OVCHHMembers, OVCHealth, OVCEducation
 from cpovc_registry.functions import get_attached_ous, get_orgs_child
+from cpovc_registry.models import RegPersonsSiblings
+from cpovc_registry.templatetags.app_filters import gen_value, vals
 
 
 @login_required(login_url='/')
@@ -16,14 +18,18 @@ def fetch_forms(request):
 
 @login_required(login_url='/')
 def fetch_data(request):
-	attached_org_units = get_attached_ous(request)
-	org_units = get_orgs_child(attached_org_units, 1)
+	user_orgs = request.user.reg_person.regpersonsorgunits_set.values()
+	org_units = []
+
+	for org in user_orgs:
+		if not org['is_void']:
+			org_units.append(org['org_unit_id'])
 
 	# access performance for this: upper bound of this: How many ovcs per ancestor org
 	ovcs_for_org = OVCRegistration.objects.filter(
 		is_void=False,
 		is_active=True,
-		child_cbo_id__in=org_units)
+		child_cbo_id__in=org_units).order_by('-id')[:1000]  # limit to 1000
 
 	ovc_data = {}
 
@@ -33,6 +39,7 @@ def fetch_data(request):
 		full_name = ovc.person.all_names.replace(' ', '')
 		full_name = ''.join([str(ord(i)) for i in full_name])
 
+
 		key = '{full_name}_{reg_person_id}_{ovc_reg_id}'.format(
 			full_name=full_name,
 			reg_person_id=ovc.person.id,
@@ -41,6 +48,7 @@ def fetch_data(request):
 		ovc_data[key] = base64.b64encode(json.dumps({
 			'id': key,
 			'person_id': ovc.person_id,
+			'registration_date': ovc.registration_date.strftime('%d/%m/%Y'),
 			'org_unique_id': ovc.org_unique_id,
 			'first_name': ovc.person.first_name,
 			'surname': ovc.person.surname,
@@ -50,12 +58,96 @@ def fetch_data(request):
 			'child_chv_full_name': ovc.child_chv.full_name,
 			'caretake_full_name': ovc.caretaker.full_name,
 			'org_unt_name': ovc.child_cbo.org_unit_name,
-			'is_active': 'Active' if ovc.is_active else 'Exited'
+			'is_active': 'Active' if ovc.is_active else 'Exited',
+			'immunization_status': gen_value(ovc.immunization_status, vals),
+			'school_level': gen_value(ovc.school_level, vals),
+			'hiv_status': ovc.hiv_status,
+			'suppressed': ovc.hiv_status if ovc.hiv_status == "HSTP" else "N/A",
+
+			# facility details
+			'facility': _get_ovc_facility_details(ovc),
+
+			# school details
+
+			'school': _get_ovc_school_details(ovc),
+
+			# house hold members
+			'household_members': _get_ovc_household_members(ovc)
 		}))
 
 	return JsonResponse({
 		'data': ovc_data
 	})
+
+
+def _get_ovc_school_details(ovc):
+	if ovc.school_level == "SLNS":
+		return {}
+
+	try:
+		school = OVCEducation.objects.get(person_id=ovc.person.id, is_void=False)
+		return {
+			'school_name': school.school.school_name,
+			'school_class': school.school_class,
+			'admission_type': school.admission_type
+		}
+	except OVCEducation.DoesNotExist as e:
+		return {}
+
+
+def _get_ovc_facility_details(ovc):
+	if ovc.hiv_status != 'HSTP':
+		return {}
+
+	try:
+		health = OVCHealth.objects.get(person_id=ovc.person.id)
+		return {
+			'name': health.facility.facility_name,
+			'art_status': health.art_status,
+			'date_linked': health.date_linked.strftime('%d/%m/%Y'),
+			'ccc_number': health.ccc_number
+		}
+	except OVCHealth.DoesNotExist as e:
+		return {}
+
+
+def _get_ovc_household_members(ovc):
+	ovc_reg_id = ovc.person.id
+	ovc_household = OVCHHMembers.objects.get(is_void=False, person_id=ovc_reg_id)
+
+	if not ovc_household:
+		return []
+
+	member_types = {
+		'TBVC': 'Sibling',
+		'TOVC': 'Enrolled OVC'
+	}
+
+	def _is_household_head(member):
+		if not member.hh_head:
+			if member.member_type == 'TBVC' or member.member_type == 'TOVC':
+				return "N/A"
+			else:
+				"No"
+		else:
+			return "Yes({})".format(ovc_household.house_hold.head_identifier)
+
+	# Get HH members
+	household_id = ovc_household.house_hold.id
+	household_members = OVCHHMembers.objects.filter(
+		is_void=False, house_hold_id=household_id).order_by("-hh_head")
+	household_members = household_members.exclude(person_id=ovc_reg_id)[:10]  # limit 10
+
+	return [{
+		'first_name': member.person.first_name,
+		'surname': member.person.surname,
+		'age': member.person.age,
+		'type': member_types.get(member.member_type, 'Parent/Guardian') ,
+		'phone_number': member.person.des_phone_number,
+		'alive': 'Yes' if member.member_alive == 'AYES' else 'No',
+		'hiv_status': member.hiv_status,
+		'household_head': _is_household_head(member)
+	} for member in household_members]
 
 
 @login_required(login_url='/')
