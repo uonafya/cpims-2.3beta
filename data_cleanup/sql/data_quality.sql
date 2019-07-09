aecea324c739db2a85dd7aaf159a67e0a34a5da8
= <=>
2019-07-09 12:10:43 +0300

    Use materialized view instead of normal view.

--- data_cleanup/sql/data_quality.sql
+++ data_cleanup/sql/data_quality.sql
@@ -1,6 +1,6 @@
-drop view data_quality_view;
+DROP MATERIALIZED VIEW IF EXISTS data_quality_view;
 
-create view data_quality_view as select 
+create MATERIALIZED view data_quality_view as select 
  ovc_registration.id as ovc_registration_id,
  ovc_registration.registration_date,
  ovc_registration.has_bcert,
