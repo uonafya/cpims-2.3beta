8dbc081d0c1904a4541afff55c2c9a1fb48a3d94
Collins Liyosi <collinskivale@gmail.com>
2019-07-09 17:56:36 +0300

    Reformat code to use project stylinh

--- cpovc_main/context_processors.py
+++ cpovc_main/context_processors.py
@@ -2,11 +2,11 @@ from django.conf import settings
 
 
 def global_settings(request):
-	"""
-	Inject particular settings in request to be accessed in templates
-	:param request:
-	:return:
-	"""
-	return {
-		'OFFLINE_MODE_CAPABILITY_ENABLED': settings.OFFLINE_MODE_CAPABILITY_ENABLED
-	}
+    """
+    Inject particular settings in request to be accessed in templates
+    :param request:
+    :return:
+    """
+    return {
+        'OFFLINE_MODE_CAPABILITY_ENABLED': settings.OFFLINE_MODE_CAPABILITY_ENABLED
+    }
