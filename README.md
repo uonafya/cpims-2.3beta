a37369be89c9484a289b2280690dde881bdb01b2
Brian Marika <mwaura.marika@gmail.com>
2019-07-10 15:58:18 +0300

    Update tests.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ CPIMS v 2.2
 [![Build Status](https://travis-ci.org/uonafya/cpims-2.3beta.svg?branch=master)](https://travis-ci.org/uonafya/cpims-2.3beta)
 
 ### Running Tests
+`$ pip install requirements/dev.txt` while on the root folder of the project
 `$ python manage.py test --settings=cpims.test_settings`
--- cpovc_main/tests/test_models.py
+++ cpovc_main/tests/test_models.py
@@ -1,12 +1,31 @@
 from django.test import TestCase
 from model_mommy import mommy
 
-from cpovc_main.models import SchoolList
+from cpovc_main.models import SchoolList, SetupGeography 
 
 
 class TestSchoolListModel(TestCase):
     """Tests for the SchoolList model """
     def test_school_creation(self):
-        mommy.make(SchoolList, _quantity=5)
-        self.assertEqual(5, SchoolList.objects.count())
-    
+        ward = mommy.make(
+            SetupGeography, 
+            area_name='Makongeni', area_code='GWRD'
+        )
+        sub_county = mommy.make(
+            SetupGeography, 
+            area_name='Makongeni Sub County', area_code='GDIS'
+        )
+        data = {
+            'school_name': 'Kenyatta High School',
+            'type_of_school': 'SLSE',
+            'school_ward': ward,
+            'school_subcounty': sub_county
+        }
+        school = mommy.make(SchoolList, **data)
+        self.assertEqual(1, SchoolList.objects.count())
+        self.assertEqual(data.get('school_name'), school.school_name)
+        self.assertEqual(
+            data.get('type_of_school'), school.type_of_school)
+        self.assertEqual('Makongeni', school.school_ward.area_name)
+        self.assertEqual(
+            'Makongeni Sub County', school.school_subcounty.area_name)
