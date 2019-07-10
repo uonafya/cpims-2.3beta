e97d94db7fceb93b5928add07dd5514a7b5385f1
= <=>
2019-07-10 09:32:10 +0300

    Add Hiv status and Art Status fields in the csv export.

--- bin/export_data.sh
+++ bin/export_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-psql -h $1  -U $2 -d $3 -o $4 -c 'select ovc_registration_id, age, first_name from data_quality_view'
+psql -h $1  -U $2 -d $3 -o $4 -c 'select first_name, age, hiv_status, art_status from data_quality_view'
