504ba58d0dda571a7f068de7f592f42f65eab9f1
Brian Marika <mwaura.marika@gmail.com>
2019-07-11 20:21:08 +0300

    Add more filters.
    - gender
    - is_disabled
    - is_ovc
    - has birth_cert

--- data_cleanup/views.py
+++ data_cleanup/views.py
@@ -17,7 +17,7 @@ class DataQualityView(TemplateView):
     template_name = 'data_cleanup/filter.html'
     paginate_by =  10
     context_object_name = "data"
-    
+
     def get_context_data(self, **kwargs):
         context = super(
             DataQualityView, self).get_context_data(**kwargs)
@@ -35,7 +35,7 @@ class DataQualityView(TemplateView):
 
     def get_queryset(self, *args, **kwargs):
         return []
-    
+
     def get(self, *args, **kwargs):
         if self.request.GET.dict().get('export', False):
             return self.export_data(*args, **kwargs)
@@ -51,7 +51,11 @@ class DataQualityView(TemplateView):
         school_level = self.request.POST.get('school_level')
         hiv_status = self.request.POST.get('hiv_status')
         art_status = self.request.POST.get('art_status')
-        
+        gender = self.request.POST.get('gender')
+        is_disabled = self.request.POST.get('is_disabled')
+        is_ovc = self.request.POST.get('is_ovc')
+        has_bcert = self.request.POST.get('has_bcert')
+
         if age:
             if  age_operator == '-' and age_operator != '0':
                 ages =  age.split('-')
@@ -59,7 +63,7 @@ class DataQualityView(TemplateView):
                     error = 'Please supply the min and max age e.g 19-20'
                     context['error'] = error
                     return TemplateResponse(
-                    self.request, self.template_name, context) 
+                    self.request, self.template_name, context)
                 else:
                     try:
                         min_age = int(ages[0])
@@ -70,27 +74,44 @@ class DataQualityView(TemplateView):
                         context['error'] = 'Please use numbers for age'
                         return TemplateResponse(
                             self.request, self.template_name, context)
-                    
+
             elif age_operator == '=':
                 queryset =  queryset.filter(age=age)
             elif age_operator == '>':
-                queryset =  queryset.filter(age__gt=age) 
+                queryset =  queryset.filter(age__gt=age)
             elif  age_operator == '<':
                 queryset = queryset.filter(age__lt=age)
-        
 
         if school_level and school_level != '0':
             queryset = queryset.filter(school_level=school_level)
-        
+
         if hiv_status and hiv_status != '0':
             queryset = queryset.filter(hiv_status=hiv_status)
 
         if art_status and art_status != '0':
             queryset = queryset.filter(art_status=art_status)
+
+        if is_disabled and is_disabled == 'True':
+            queryset = queryset.filter(is_disabled=True)
+
+        if is_disabled and is_disabled == 'False':
+            queryset = queryset.filter(is_disabled=False)
+
+        if gender:
+            queryset = queryset.filter(sex_id=gender)
+
+        if is_ovc and is_ovc != '0':
+            queryset = queryset.filter(designation=is_ovc)
+
+        if has_bcert and has_bcert == 'True':
+            queryset = queryset.filter(has_bcert=True)
+
+        if has_bcert and has_bcert == 'False':
+            queryset = queryset.filter(has_bcert=False)
         queryset = self.get_final_query_set(queryset)
         context['data']= queryset
         return TemplateResponse(self.request, self.template_name, context)
-    
+
     def generate_where_clause(self):
         query_dict = self.request.GET.dict()
         school_level = query_dict.get('school_level')
@@ -99,7 +120,7 @@ class DataQualityView(TemplateView):
         art_status = query_dict.get('art_status')
         hiv_status = query_dict.get('hiv_status')
         sql = "WHERE 1=1 "
-        if school_level != '0': 
+        if school_level != '0':
             sql += 'school_level={} AND '.format(school_level)
         if age:
             sql += 'age={} AND '.format(age)
@@ -121,7 +142,7 @@ class DataQualityView(TemplateView):
         where_sql = self.generate_where_clause()
         path_to_file = '/tmp/{}file.csv'.format(self.request.user)
         call('bin/export_data.sh {} {} {} {} {}'.format(
-            db_host, db_user, db_name, path_to_file, where_sql), 
+            db_host, db_user, db_name, path_to_file, where_sql),
             shell=True
         )
         file_name = '/tmp/file.csv'
