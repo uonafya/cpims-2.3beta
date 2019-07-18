10704e64143a5930a2661cbc28a86c08736bfcf3
Benzer Bett <benzerbett@gmail.com>
2019-07-18 19:24:39 +0300

    fix form1b saving

--- cpovc_forms/functions.py
+++ cpovc_forms/functions.py
@@ -48,7 +48,8 @@ def create_fields(field_name=[], default_txt=False):
             'item_id', 'item_description_short', 'item_description',
             'item_sub_category')
         for value in all_list:
-            item_id = value['item_description_short']
+            item_description_short = value['item_description_short']
+            item_id = value['item_id']
             item_cat = value['item_sub_category']
             item_details = value['item_description']
             items = {'id': item_id, 'name': item_details}
