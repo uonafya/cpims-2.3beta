f9020e1874d95855a62a62d60f97d35b88682f60
Benzer <benzerbett@gmail.com>
2019-07-22 15:46:13 +0300

    Revert "Data clean up (#177)"
    This reverts commit cdb73bf2065144f943f2cf52f7c5627bc9d51584.

--- data_cleanup/urls.py
+++
@@ -1,9 +0,0 @@
-"""Data cleanup section urls."""
-from django.conf.urls import patterns, url
-
-from .views import DataQualityView
-# This should contain urls related to registry ONLY
-urlpatterns = patterns(
-    'data_cleanup.views',
-    url(r'^filter/$', DataQualityView.as_view(), name='data_cleanup')
-)
