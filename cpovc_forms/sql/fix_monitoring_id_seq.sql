dc18692054f4802d47a25ea1bdd3d051fb1b01ce
steve <steviewez@gmail.com>
2019-08-03 18:16:09 +0300

    additional query to set seq value start

--- cpovc_forms/sql/fix_monitoring_id_seq.sql
+++ cpovc_forms/sql/fix_monitoring_id_seq.sql
@@ -3,3 +3,8 @@
   owned by ovc_monitoring.monitoring_id; */
 
 alter table ovc_monitoring alter column monitoring_id set default nextval('public.ovc_monitoring_monitoring_id_seq'::regclass);
+
+/* Set value of sequence to last value in table in the case that it is not set */
+/*
+SELECT setval('ovc_monitoring_monitoring_id_seq', 100, true);
+ */
