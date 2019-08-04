5d327da91ea1d7a87f65664276bcc31ffa2aa1b6
Brian Marika <mwaura.marika@gmail.com>
2019-08-04 14:01:49 +0300

    Implement reset button for data quality filters.

--- data_cleanup/urls.py
+++ data_cleanup/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, url
 
 from .views import DataQualityView
-# This should contain urls related to registry ONLY
+
 urlpatterns = patterns(
     'data_cleanup.views',
     url(r'^filter/$', login_required(DataQualityView.as_view()), name='data_cleanup')
