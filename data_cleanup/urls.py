437955387ab8fdbd1cb3addaf026e2c308313079
Brian Marika <mwaura.marika@gmail.com>
2019-08-14 21:49:41 +0300

    Separate the case plan and the services templates.

--- data_cleanup/urls.py
+++ data_cleanup/urls.py
@@ -2,9 +2,12 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, url
 
-from .views import DataQualityView
+from .views import DataQualityView, CasePlanDataQualityView
 
 urlpatterns = patterns(
     'data_cleanup.views',
-    url(r'^filter/$', login_required(DataQualityView.as_view()), name='data_cleanup')
+    url(r'^filter/$', login_required(
+        DataQualityView.as_view()), name='data_cleanup'),
+    url(r'^filter/case_plan$', login_required(
+        CasePlanDataQualityView.as_view()), name='data_cleanup_case_plan')
 )
