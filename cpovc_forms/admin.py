f1a04b49b1c84bb94feaa577bd9604b073bc0ea3
Stephen Waweru <steviewez@gmail.com>
2019-10-02 11:04:42 +0300

    Add admin page for ovc care questions (#291)

--- cpovc_forms/admin.py
+++ cpovc_forms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import OVCCareQuestions
+
 
 # Register your models here.
+class OVCCareQuestionsAdmin(admin.ModelAdmin):
+    list_display = ('code', 'question', 'question_type', 'domain', 'question_text', 'is_void')
+    search_fields = ['code', 'question', 'domain', 'question_type']
+
+
+admin.site.register(OVCCareQuestions, OVCCareQuestionsAdmin)
