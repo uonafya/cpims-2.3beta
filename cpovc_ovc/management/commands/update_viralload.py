ee92f4a21ff678fc596f682ed7569cf57b97cf91
Thomas <thomasbshop@gmail.com>
2020-04-27 05:43:07 +0300

    Add custom managment command 'python manage.py update_viralload' to update viral load

---
+++ cpovc_ovc/management/commands/update_viralload.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from utilities.update_ovc_viralload import UpdateViralLoad
+
+class Command(BaseCommand):
+	help = 'Updates viral load for each existing and non-existing \
+			OVCs'
+
+	def add_arguments(self, parser):
+		pass
+
+	def handle(self, *args, **options):
+		UpdateViralLoad().loop_through_data()
+		self.stdout.write('----- Updated viral loads -----')
+		# self.stdout.write('Sent {} reminders'.format(len(emails)))
