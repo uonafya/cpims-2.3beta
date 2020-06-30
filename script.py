11c8942ce711bd0934a08ba4277fce9984b28be3
richie <richardjohnzranger@gmail.com>
2020-06-30 12:41:17 +0300

    working dreams services

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import csv
 from cpovc_main.models import SetupList
-with open('/home/richie/Desktop/Dreams-Sheet1.csv') as csv_file:
+with open('/home/richie/Desktop/fixtures3 - Sheet1.csv') as csv_file:
 	dict_reader = csv.DictReader(csv_file)
 	for row in dict_reader:
          item_category = row['item_category']
@@ -9,12 +9,12 @@ with open('/home/richie/Desktop/Dreams-Sheet1.csv') as csv_file:
          isc = row['item_sub_category']
          itd = row['item_id']
          ids = row['item_description_short']
-         order = row['the_order']
-	 item_dis = row['item_description'] 
+        #  order = row['the_order']
+         item_dis = row['item_description'] 
 	 
 	 
          setuplist = SetupList(item_description=item_dis,item_id=itd,item_description_short=ids,item_category=item_category,item_sub_category=isc
-                              ,field_name=fn,the_order=order,user_configurable=uc,)
+                              ,field_name=fn,user_configurable=uc,)
          setuplist.save()
 
 
