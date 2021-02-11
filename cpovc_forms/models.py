16a8c50e6c3fd8681c1178f37feae83e7674d672
steve <steviewez@gmail.com>
2021-02-11 11:26:07 +0300

    add case stage field to OVCCaseRecord

--- cpovc_forms/models.py
+++ cpovc_forms/models.py
@@ -60,6 +60,7 @@ class OVCCaseRecord(models.Model):
     case_remarks = models.CharField(max_length=1000, null=True)
     date_of_summon = models.DateField(null=True)
     summon_status = models.NullBooleanField(null=True, default=None)
+    case_stage = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'ovc_case_record'
